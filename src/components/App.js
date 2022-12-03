32e8a0808f8b002c5cae47e233f7f66a0b142f33
nizhantiw <nizhantiw@gmail.com>
2022-12-03 20:06:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,51 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here 
-  
 
-  
+  const [current, setCurrent] = useState('OFF');
+  const [current2, setCurrent2] = useState('ON');
+
+  const handleClick = (event) => {
+
+    if (event.target.id == 'btn1') {
+      if (event.target.innerText == 'ON' && event.target.id == 'btn1') {
+        setCurrent(() => 'OFF');
+        setCurrent2(() => 'ON');
+      }
+      else {
+        setCurrent(() => 'ON');
+        setCurrent2(() => 'OFF');
+      }
+    }
+    else {
+      if (event.target.value == 'OFF') {
+        setCurrent(() => 'OFF');
+        setCurrent2(() => 'ON');
+      }
+      else {
+        setCurrent(() => 'ON');
+        setCurrent2(() => 'OFF');
+      }
+    }
+
+  }
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+      <button id="btn1" onClick={handleClick} value={current}  >
+        {current}
       </button>
       <br />
       <br />
-      <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+      <button id="btn2" onClick={handleClick} value={current2}   >
+        {current2}
       </button>
     </div>
   );
